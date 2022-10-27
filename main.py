375714fde2d60ee0a0db1a7e6936dc99f992d2c6
Benjamin Fuller <benisfull@gmail.com>
2022-10-26 18:01:35 -0700

    add and prepare imgs for game and level 1 layout, movement system prototype

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import sys
 from math import floor, ceil
 from pygame.locals import *
+from layouts import level_1_layout
 
 pygame.mixer.pre_init()
 pygame.init()
@@ -18,10 +19,14 @@ center_w = width / 2
 center_h = height / 2
 window = pygame.display.set_mode((width, height), flags)
 
+two_face_img = pygame.image.load("imgs/twoface.png")
+two_face_img = pygame.tranform.scale(two_face_img, (tile, tile))
 menu_bg = pygame.image.load("imgs/twofacemenuBG.png").convert()
 menu_bg = pygame.transform.scale(menu_bg, (width, height))
 menu_bg_width = menu_bg.get_width()
 menu_tiles = ceil(width / menu_bg_width) + 1  # creates tiles for menu screen scroll
+
+#level_1_bg = pygame.image.load...
 
 title_font = pygame.font.Font("fonts/VCR_OSD_MONO.ttf", 125)
 pygame.display.set_caption('two-face.')  # caption of window
@@ -45,9 +50,13 @@ class Game:
         self.speed = 1 / 32
         self.dir = self.new_dir = 0
         self.two_face_angle = 0
+        self.two_face_rect = Rect(tile, tile, tile, tile)
         self.stopped = False
         self.state = "main_menu"
+        self.game_scroll = [0, 0]
+        self.wiggle = [0, 0]
         self.menu_scroll = 0
+        # self.level_bg = level_1_bg
         self.i = 0
         self.keyScore = 0
 
@@ -124,6 +133,44 @@ class Game:
                     pygame.quit()  # quit on keystroke q
                     sys.exit()
 
+    def boardCreate(self):
+        # wiggle room: the size of the background level image - the size of the screen
+        self.wiggle[0] = self.level_bg.get_size()[0] - width
+        self.wiggle[1] = self.level_bg.get_size()[1] - height
+
+        # game screen scroll: screen move effect centered around player but stops at screen edges
+        self.game_scroll[0] += (self.two_face_rect.x - self.game_scroll[0] - center_w) / 40
+        self.game_scroll[1] += (self.two_face_rect.y - self.game_scroll[1] - center_h) / 40
+
+        # game will always stop scrolling at the borders of the game map
+        if self.game_scroll[0] < 0:
+            self.game_scroll[0] = 0
+        if self.game_scroll[1] < 0:
+            self.game_scroll[1] = 0
+        if self.game_scroll[0] > self.wiggle[0]:
+            self.game_scroll[0] = self.wiggle[0]
+        if self.game_scroll[1] > self.wiggle[1]:
+            self.game_scroll[1] = self.wiggle[1]
+
+        window.blit(self.levelBG, (0 - self.game_scroll[0], 0 - self.game_scroll[1]))  # blit BG image for current level
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                # if self.board[i][j] == 0:  # 0 = background
+                if self.board[i][j] == 1:  # 1 = bug
+                    bugRect = bugImage.get_rect(center=(j * tile + tile // 2, i * tile + tile // 2))
+                    window.blit(bugImage, (bugRect.x - self.game_scroll[0], bugRect.y - self.game_scroll[1]))
+                # if self.board[i][j] == 2:  # 2 = empty, movable spot
+                if self.board[i][j] == 3:  # 3 = rainbow bug
+                    bugRect = bugImage4.get_rect(center=(j * tile + tile // 2, i * tile + tile // 2))
+                    self.shakingImage(bugImage4, bugRect.x - self.game_scroll[0], bugRect.y - self.game_scroll[1])
+
+        two_face_rotate = pygame.transform.rotate(two_face_img, self.two_face_angle)
+        self.two_face_rect = two_face_img.get_rect(center=(floor(self.two_face[1] * tile + tile // 2),
+                                                           floor(self.two_face[0] * tile + tile // 2)))
+        window.blit(two_face_rotate, (self.two_face_rect.x - self.game_scroll[0],
+                                   self.two_face_rect.y - self.game_scroll[1]))
+        pygame.display.update()
+
     def mainMenu(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -157,7 +204,7 @@ class Game:
     def prelevel1(self):
         window.fill((0, 0, 0))
         prelevel1_title = title_font.render("begin.", False, (207, 198, 184))
-        if 300 < self.i < 600:
+        if 200 < self.i < 500:
             window.blit(prelevel1_title, (center_w - 125, center_h - 125))
         pygame.display.update()
         for event in pygame.event.get():
@@ -165,21 +212,21 @@ class Game:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                # if event.key == pygame.K_p:
-                # self.i = 0  # resets global iterator to 0 for use elsewhere
-                # self.state = "level_1"
+                if event.key == pygame.K_p:
+                    self.i = 0  # resets global iterator to 0 for use elsewhere
+                    self.state = "level_1"
                 if event.key == pygame.K_q:
                     pygame.quit()  # quit on keystroke q
                     sys.exit()
-        if self.i > 900:  # 5 seconds max on transition screen; can be skipped with p
+        if self.i > 700:  # 7 seconds max on transition screen; can be skipped with p
             self.i = 0  # resets global iterator to 0 for use elsewhere
             self.state = "level_1"
         self.i += 1
 
     def level1(self):
         self.keyGetter()
-        #self.twoFaceMovement(level_1_layout)
-        self.makeBoard()
+        self.twoFaceMovement(level_1_layout)
+        self.boardCreate()
         if self.keyScore == 1:
             self.i = 0
             self.state = "pre-level_2"
